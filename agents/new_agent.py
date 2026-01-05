339b7e6ac3a28f59f8ee0c6a313f1109f5854979
une_glace <une_glace@sjtu.edu.cn>
2026-01-05 16:02:45 +0800

    提交算法代码

--- agents/new_agent.py
+++ agents/new_agent.py
@@ -2,7 +2,17 @@ import math
 import pooltool as pt
 import numpy as np
 from pooltool.objects import PocketTableSpecs, Table, TableType
+import copy
+import os
 from datetime import datetime
+import random
+import signal
+# from poolagent.pool import Pool as CuetipEnv, State as CuetipState
+# from poolagent import FunctionAgent
+
+from bayes_opt import BayesianOptimization, SequentialDomainReductionTransformer
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.gaussian_process.kernels import Matern
 
 from .agent import Agent
 
